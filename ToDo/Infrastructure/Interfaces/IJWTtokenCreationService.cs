a6c16191b06c647bd705d0b66c6fe306e24137a4
Mohammad abusabha <56313941+MohammadAbusabha@users.noreply.github.com>
2025-12-28 19:58:26 +0300

    Delete ToDo directory

--- ToDo/Infrastructure/Interfaces/IJWTtokenCreationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ToDo.Core.Entities;
-
-namespace ToDo.Infrastructure.Interfaces
-{
-    public interface IJWTtokenCreationService
-    {
-        string CreateJWTtoken(ApplicationUser user);
-    }
-}
